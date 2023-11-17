93a12ed183ea840a878324f55d50016e90177656
Lele1818 <148886814+Lele1818@users.noreply.github.com>
2023-11-18 00:43:42 +0700

    Add files via upload

---
+++ src/views/hompage/homepage.js
@@ -0,0 +1,36 @@
+import Calendarr from "../../components/calendar/calendar";
+import Menu from "../../components/Menu";
+import Sidebar from "../../components/sidebar/sidebar";
+import Sidebarr from "../../components/sidebar2/sidebar";
+import "bootstrap/dist/css/bootstrap.css";
+import { Button, Alert, Col, Row, Container } from "react-bootstrap";
+import SearchBar from "../../components/searchbar";
+import "./homepage.css";
+import Kanban from "../../components/kanban";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div>
+        <Menu className="menu" />
+        {/* <div className="searchbar">
+          <SearchBar></SearchBar>
+        </div> */}
+    
+      </div>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Sidebarr className="sidebar" />
+          </Col>
+          <Col sm={10}>
+            <Calendarr />
+            {/* Muốn thêm thành phần gì thì bỏ vào đây */}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Homepage;
