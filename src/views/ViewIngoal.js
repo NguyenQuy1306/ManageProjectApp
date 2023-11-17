3b3ac28ddf2e55278ca47418ee954dd1e37c4423
Lele1818 <148886814+Lele1818@users.noreply.github.com>
2023-11-18 00:37:07 +0700

    Add files via upload
    tao folder viewIngoal

---
+++ src/views/ViewIngoal.js
@@ -0,0 +1,32 @@
+import Menu from "../../components/Menu";
+import Sidebarr from "../../components/sidebar2/sidebar";
+import "bootstrap/dist/css/bootstrap.css";
+import { Button, Alert, Col, Row, Container } from "react-bootstrap";
+import SearchBar from "../../components/searchbar";
+import ListGoal from "../../components/listGoal";
+import "./ViewIngoal.css";
+function Viewgoal() {
+  return (
+    <div className="goal">
+      <div>
+        <Menu className="menu" />
+        {/* <div className="searchbar">
+          <SearchBar></SearchBar>
+        </div> */}
+      </div>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Sidebarr className="sidebar" />
+          </Col>
+          <Col sm={10}>
+            {/* Muốn thêm thành phần gì thì bỏ vào đây */}
+            <ListGoal />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Viewgoal;
