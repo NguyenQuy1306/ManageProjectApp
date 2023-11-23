57b6a12eb2b7546add50aa3a33fe4b6eb4414f40
Lele1818 <148886814+Lele1818@users.noreply.github.com>
2023-11-23 17:39:00 +0700

    Update App.js (them tam profile)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Sidebar from "./components/sidebar2/sidebar";
 import Viewsection from "./views/viewInsection/ViewInsection";
 import Homepage from "./views/hompage/homepage";
 import Viewproject from "./views/viewInproject/ViewInproject";
+import Viewgoal from "./views/viewIngoal/ViewIngoal";
+import ProfilePage from "./views/viewProfile/viewProfile";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +16,8 @@ function App() {
           <Route path="/Homepage" element={<Homepage />} />
           <Route path="/Homepage" element={<Homepage />} />
           <Route path="/home" element={<Sidebar />} />
+          <Route path="/goal" element={<Viewgoal />} />
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/project1" exact element={<Viewproject />} />
           <Route path="/project2" exact element={<Viewproject />} />
           <Route path="/project1/section_1" exact element={<Viewsection />} />
